7f5b5d3c2a5857d4aa1ec9c9ddd6830db2dce545
Mayank Jha <mayankxjha@gmail.com>
2023-12-26 03:35:22 +0530

    Indentation on output

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,97 +6,110 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
-	"math/rand"
 	// "reflect"
 	// "math/rand"
 	// "strconv"
 	"github.com/gorilla/mux"
 )
-type Movie struct{
-	ID int
-	Title string
-	ISBN string
+type Movie struct {
+	ID       int
+	Title    string
+	ISBN     string
 	Director Director
 }
-
-type Director struct{
+type Director struct {
 	Fname string
 	Lname string
 }
-func populateMovies(path string) (data []uint8){
+func populateMovies(path string) (data []uint8) {
 	file, err := os.Open("movies.txt")
 	if err != nil {
 		fmt.Println("Error reading file:", err)
-		return 
+		return
 	}
 	defer file.Close()
 	fileInfo, err := file.Stat()
 	if err != nil {
 		fmt.Println("Error getting file info:", err)
-		return 
+		return
 	}
 	fileSize := fileInfo.Size()
 	data = make([]byte, fileSize)
 	_, err = file.Read(data)
 	if err != nil {
 		fmt.Println("Error reading file:", err)
-		return 
+		return
 	}
-	return 
+	return
 }
-func structData(data []uint8) (movies []Movie){
+func populateTxt(path string, movies []Movie){
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	defer file.Close()
+	_, err = file.Write([]byte(jsonData(movies)))
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+}
+func structData(data []uint8) (movies []Movie) {
 	err := json.Unmarshal(data, &movies)
 	if err != nil {
 		return
 	}
 	return movies
 }
-func jsonData(data Movie) (jsonString string){
-	d, err := json.Marshal(data)
+func jsonData(data interface{}) (jsonString string) {
+	indentedData, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
-		jsonString = fmt.Sprintln("Error marshaling to JSON:", err)
+		fmt.Println("Error marshaling JSON:", err)
 		return
 	}
-	jsonString = string(d)
+	jsonString = string(indentedData)
 	return jsonString
 }
-func keyGen(params map[string]string) (key int){
+func keyGen(params map[string]string) (key int) {
 	key, _ = strconv.Atoi(params["ID"])
-	key-=1
+	key -= 1
 	return key
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request){
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello there, General Kenobi")
 }
 func getAllMovies(w http.ResponseWriter, r *http.Request) {
 	movies := structData(populateMovies("movies.txt"))
-	for _, movie := range(movies){
+	for _, movie := range movies {
 		fmt.Fprintf(w, "%s\n\n", jsonData(movie))
-		
 	}
+	populateTxt("moviesExp.txt", movies)
 }
 
-func getMovie(w http.ResponseWriter, r *http.Request){
+func getMovie(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	key := keyGen(params)
 	movies := structData(populateMovies("movies.txt"))
 	fmt.Fprintf(w, "%s", jsonData(movies[key]))
 }
 
-func addMovie(w http.ResponseWriter, r *http.Request){
+func addMovie(w http.ResponseWriter, r *http.Request) {
 	movies := structData(populateMovies("movies.txt"))
 	w.Header().Set("Content-type", "application/json")
 	var Mov Movie
 	_ = json.NewDecoder(r.Body).Decode(&Mov)
 	Mov.ID = rand.Intn(1000000)
 	movies = append(movies, Mov)
+	populateTxt("moviesExp.txt", movies)
 	json.NewEncoder(w).Encode(Mov)
 }
-func main(){
+func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/hello", http.StatusMovedPermanently)
@@ -109,7 +122,7 @@ func main(){
 	// r.HandleFunc("/movies/{ID}", deleteMovie)
 	http.Handle("/", r)
 	fmt.Println("Starting server at port 8080")
-	if err:=http.ListenAndServe(":8080", nil); err!=nil{
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
