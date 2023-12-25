bf006f3509a02a7faaa735358edca7852583b915
Mayank Jha <mayankxjha@gmail.com>
2023-12-25 17:56:41 +0530

    Global Varibles pending

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"math/rand"
 	// "reflect"
 	// "math/rand"
 	// "strconv"
@@ -45,30 +47,64 @@ func populateMovies(path string) (data []uint8){
 	}
 	return 
 }
+func structData(data []uint8) (movies []Movie){
+	err := json.Unmarshal(data, &movies)
+	if err != nil {
+		return
+	}
+	return movies
+}
+func jsonData(data Movie) (jsonString string){
+	d, err := json.Marshal(data)
+	if err != nil {
+		jsonString = fmt.Sprintln("Error marshaling to JSON:", err)
+		return
+	}
+	jsonString = string(d)
+	return jsonString
+}
+func keyGen(params map[string]string) (key int){
+	key, _ = strconv.Atoi(params["ID"])
+	key-=1
+	return key
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Hello there, General Kenobi")
 }
 func getAllMovies(w http.ResponseWriter, r *http.Request) {
-	data := populateMovies("movies.txt")
-	for _, movie :=range(data){
-		fmt.Fprintf(w, "%s", string(movie))
+	movies := structData(populateMovies("movies.txt"))
+	for _, movie := range(movies){
+		fmt.Fprintf(w, "%s\n\n", jsonData(movie))
+		
 	}
 }
+
+func getMovie(w http.ResponseWriter, r *http.Request){
+	params := mux.Vars(r)
+	key := keyGen(params)
+	movies := structData(populateMovies("movies.txt"))
+	fmt.Fprintf(w, "%s", jsonData(movies[key]))
+}
+
+func addMovie(w http.ResponseWriter, r *http.Request){
+	movies := structData(populateMovies("movies.txt"))
+	w.Header().Set("Content-type", "application/json")
+	var Mov Movie
+	_ = json.NewDecoder(r.Body).Decode(&Mov)
+	Mov.ID = rand.Intn(1000000)
+	movies = append(movies, Mov)
+	json.NewEncoder(w).Encode(Mov)
+}
 func main(){
-	var movies []Movie
-	err := json.Unmarshal(populateMovies("movies.txt"), &movies)
-	if err != nil {
-		fmt.Println("Error unmarshaling JSON:", err)
-		return
-	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/hello", http.StatusMovedPermanently)
 	})
 	r.HandleFunc("/hello", helloHandler)
 	r.HandleFunc("/movies", getAllMovies)
-	// r.HandleFunc("/movies", addMovie)
-	// r.HandleFunc("/movies/{ID}", getMovie)
+	r.HandleFunc("/movies", addMovie)
+	r.HandleFunc("/movies/{ID}", getMovie)
 	// r.HandleFunc("/movies/{ID}", updateMovie)
 	// r.HandleFunc("/movies/{ID}", deleteMovie)
 	http.Handle("/", r)
